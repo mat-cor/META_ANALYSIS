9660defb47ab2a813ba0804492339ceaad577a6d
mat-cor <mattia.cordioli01@universitadipavia.it>
2020-12-22 17:52:18 +0200

    fix pkgs import

--- scripts/combine_zscores.R
+++ scripts/combine_zscores.R
@@ -5,7 +5,8 @@ packs <- c("optparse","data.table","R.utils","dplyr")
 for (p in packs) {
   if( !require(p, character.only = T)) {
     print(p)
-    install.packages( p,  repos = c(CRAN = "https://cloud.r-project.org") )  
+    install.packages( p,  repos = c(CRAN = "https://cloud.r-project.org") )
+    require(p, character.only = T)
   }
 }
 
