5eff20f359a9ff5d0a6e9afdeae8aad8fb57b1fc
mat-cor <mattia.cordioli01@universitadipavia.it>
2021-03-31 10:12:21 +0300

    update script

--- scripts/qqplot_rev.R
+++ scripts/qqplot_rev.R
@@ -15,6 +15,8 @@ option_list = list(
               help="dataset file name", metavar="character"),
   make_option(c("-o", "--out"), type="character",
               help="output file name [default= %default]", metavar="character"),
+  make_option(c("-t", "--title"), type="character",
+              help="output file name [default= %default]", metavar="character"),
   make_option(c("-c","--chrcol"), type="character", default="CHR",
               help="chromosome column [default= %default]", metavar="character"),
   make_option(c("-p","--pval_col"), type="character", default="P",
@@ -35,6 +37,7 @@ opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser, positional_arguments=0);
 
 file <- opt$options$file
+
 print(paste("reading file:", file))
 
 data <- fread(file, header=T)
@@ -52,7 +55,7 @@ print( summary( data[[chr_col]] ) )
 pcols <- unlist(strsplit(opt$options$pval_col,","))
 
 output_prefix=file
-title_text <- sub(".txt.*", "", file)
+title_text <- opt$options$title
 
 if( !is.null(opt$options$out)) {
   output_prefix=opt$options$out
@@ -83,8 +86,8 @@ data[[chr_col]] <- gsub("MT|chrMT|M|chrM","25",data[[chr_col]])
 data[[chr_col]] <- as.numeric(data[[chr_col]])
 data <- data[ !is.na(data[[chr_col]]) ]
 
-quants <- c(0.7,0.5,0.1,0.01, 0.001)
-
+#quants <- c(0.7,0.5,0.1,0.01, 0.001)
+quants <- c(0.5)
 
 for( pcol in pcols) {
   subdata <- data[ !is.na(data[[pcol]]) & is.numeric( data[[pcol]]  ) ]
@@ -97,35 +100,35 @@ for( pcol in pcols) {
   title(c(title_text, paste("\n", "\nlambda ", quants, ": ", lambda, sep="" )), line=-0.2)
   dev.off()
 
-  sink( paste(output_prefix,"_",  pcol ,"_qquantiles.txt", sep="" ) )
-  cat( paste( quants, ":", lambda, sep=""))
-  sink()
+  #sink( paste(output_prefix,"_",  pcol ,"_qquantiles.txt", sep="" ) )
+  #cat( paste( quants, ":", lambda, sep=""))
+  #sink()
 
-  print("subsetting p-vals < 0.01 for manhattan...")
+  #print("subsetting p-vals < 0.01 for manhattan...")
 
-  subdata <- subdata[ subdata[[pcol]]<0.01 & subdata[[pcol]]>0 ]
-  print( paste0("Plotting manhattan with ", nrow(subdata), " variants") )
-  print( summary(subdata[[pcol]] ))
-  png( paste(output_prefix,"_",pcol,"_manhattan.png", sep=""), width=1000, height=400)
-  logs <- -log10(subdata[[pcol]])
-  manhattan( data.table(subdata[,c(bp_col,pcol,chr_col), with=F]) , chr=chr_col, bp=bp_col, p=pcol, ylim=c( 2,max(logs)+1), main=title_text, suggestiveline=F)
-  dev.off()
+  # subdata <- subdata[ subdata[[pcol]]<0.01 & subdata[[pcol]]>0 ]
+  # print( paste0("Plotting manhattan with ", nrow(subdata), " variants") )
+  # print( summary(subdata[[pcol]] ))
+  # png( paste(output_prefix,"_",pcol,"_manhattan.png", sep=""), width=1000, height=400)
+  # logs <- -log10(subdata[[pcol]])
+  # manhattan( data.table(subdata[,c(bp_col,pcol,chr_col), with=F]) , chr=chr_col, bp=bp_col, p=pcol, ylim=c( 2,max(logs)+1), main=title_text, suggestiveline=F)
+  # dev.off()
 
 
-  print("plotting log-log manhattan")
-  loglog_p <- opt$options$loglog_pval
-  logs <- ifelse(logs < loglog_p, logs, loglog_p * log10(logs) / log10(loglog_p))
-  loglog_ylim <- opt$options$loglog_ylim
-  if (loglog_ylim==0) {
-    max_loglog <- max(logs)
-  } else {
-    max_loglog <- loglog_p * log10(loglog_ylim) / log10(loglog_p)
-  }
-  subdata[["p_scaled"]] <- 10^(-logs)
-  tick_pos <- round(seq(1, max_loglog, length.out=max_loglog))
-  tick_lab <- sapply(tick_pos, function(pos) { round(ifelse(pos < loglog_p, pos, loglog_p^(pos/loglog_p))) })
-  png( paste(output_prefix,"_",pcol,"_manhattan_loglog.png", sep=""), width=1000, height=400)
-  manhattan( data.table(subdata[,c(bp_col,"p_scaled",chr_col), with=F]) , chr=chr_col, bp=bp_col, p="p_scaled", ylim=c( 2,max_loglog), yaxt="n", main=title_text, suggestiveline=FALSE)
-  axis(2, at = tick_pos, labels=tick_lab, las=2)
-  dev.off()
+  # print("plotting log-log manhattan")
+  # loglog_p <- opt$options$loglog_pval
+  # logs <- ifelse(logs < loglog_p, logs, loglog_p * log10(logs) / log10(loglog_p))
+  # loglog_ylim <- opt$options$loglog_ylim
+  # if (loglog_ylim==0) {
+  #   max_loglog <- max(logs)
+  # } else {
+  #   max_loglog <- loglog_p * log10(loglog_ylim) / log10(loglog_p)
+  # }
+  # subdata[["p_scaled"]] <- 10^(-logs)
+  # tick_pos <- round(seq(1, max_loglog, length.out=max_loglog))
+  # tick_lab <- sapply(tick_pos, function(pos) { round(ifelse(pos < loglog_p, pos, loglog_p^(pos/loglog_p))) })
+  # png( paste(output_prefix,"_",pcol,"_manhattan_loglog.png", sep=""), width=1000, height=400)
+  # manhattan( data.table(subdata[,c(bp_col,"p_scaled",chr_col), with=F]) , chr=chr_col, bp=bp_col, p="p_scaled", ylim=c( 2,max_loglog), yaxt="n", main=title_text, suggestiveline=FALSE)
+  # axis(2, at = tick_pos, labels=tick_lab, las=2)
+  # dev.off()
 }
